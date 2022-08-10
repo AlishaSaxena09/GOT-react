f534cc94e439076def96058da0de354edf4c6b55
Alisha Saxena <alishasaxena2016@gmail.com>
2022-08-11 01:09:30 +0530

    Updated GOT

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,23 +4,21 @@ import { useParams } from "react-router-dom";
 export default function Book() {
   const { id } = useParams();
   const [book, setBook] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetch(`https://anapioficeandfire.com/api/books/${id}`)
       .then((res) => res.json())
       .then((res) => {
         setBook(res);
-        setIsLoading(false);
         console.log(res);
       });
-  }, []);
+  }, [id]);
 
   return (
     <div className=" bg-blue-800 w-full font-oswald p-10">
       <div className="  border-8 border-black m-auto text-center px-2 py-8 bg-white w-fit top-3 flex flex-wrap justif-center">
         <div className="w-1/2">
-          <img className="w-80 m-auto" src="/images/got-i.jpg"></img>
+          <img alt="got" className="w-80 m-auto" src="/images/got-i.jpg"></img>
         </div>
         <div className="w-1/2 leading-9 pt-10">
           <h1 className=" text-4xl font-extrabold font-cinzel">{book.name}</h1>
